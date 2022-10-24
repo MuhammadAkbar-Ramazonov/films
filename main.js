495b360c747326714553e273e2a14ab40e564c69
John Doe <ramazonov3600@gmail.com>
2022-10-25 00:56:46 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// Variables
+const elFilmList = document.querySelector(".film-list");
+const elFilmTemplate = document.querySelector(".film-temp").content;
+const FilmFragment = document.createDocumentFragment();
+
+// For Loop
+for (const  kino of kinolar) {
+  const elFilmTemplate = document.querySelector(".film-temp").content.cloneNode(true);
+  
+  elFilmTemplate.querySelector(".film-title").textContent = kino.title;
+  elFilmTemplate.querySelector(".film-year").textContent = kino.year;
+  elFilmTemplate.querySelector(".film-cast").textContent = kino.cast.join(", ");
+  elFilmTemplate.querySelector(".film-genres").textContent = kino.genres.join(", ");
+  
+  FilmFragment.appendChild(elFilmTemplate);
+}
+elFilmList.appendChild(FilmFragment);
+
+
+
+
+
